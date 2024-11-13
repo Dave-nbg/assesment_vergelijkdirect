f4534da1a08149cec10a71248f395868c03b6ea8
Dave-nbg <74248265+Dave-nbg@users.noreply.github.com>
2024-11-13 19:33:04 +0100

    begin of info show

--- car-comparison-form/src/components/LicensePlateComponent.vue
+++ car-comparison-form/src/components/LicensePlateComponent.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="flex justify-center mb-2">
         <label>Fill in your licenseplate</label>
-        <div class="rounded-full bg-black text-white w-5 h-5 ml-3 flex justify-center items-center">
+        <div @mouseenter="showInformation" class="informationWrapper rounded-full bg-black text-white w-5 h-5 ml-3 flex justify-center items-center">
             <p class="font-bold ">i</p>
+        </div>
+        <div className="hidden w-[200px] textDialog absolute bottom-0 left-1/2 transform -translate-x-1/2 mt-2 p-2 text-sm bg-gray-800 text-white rounded-md transition-opacity duration-300">
+            Het wachtwoord moet minimaal 1 cijfer en 1 speciaal karakter bevatten
         </div>
     </div>
     <div class="flex justify-center">
@@ -11,4 +14,8 @@
     </div>
 </template>
 
-
+<script setup>
+    const showInformation = () =>{
+        console.log("test")
+    }
+</script>
